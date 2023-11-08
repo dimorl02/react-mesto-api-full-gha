afe17528fed44b89f231dc63c06ff3a149f5d969
dimorl02 <dimorl02@gmail.com>
2023-11-08 20:22:41 +0300

    деплой 1.0

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.mesto.dimorl02.nomoredomainsroсks';
+export const BASE_URL = 'https://api.mesto.dimorl02.nomoredomainsroсks.ru';
 
 function checkResponseValidity(res) {
     if (res.ok) {

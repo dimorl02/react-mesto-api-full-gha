e27a484955a943ffeba434c8aeb3331dc7fe28c6
dimorl02 <dimorl02@gmail.com>
2023-11-08 20:52:32 +0300

    деплой 3.0

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 const settingsApi = {
-    url: 'https://api.dimorl02.nomoredomainsrocks.ru',
+    url: 'https://api.mesto.dimorl02.nomoredomainsrocks.ru',
     headers:{
       authorization: '95a01d31-8d5f-43f8-b501-fe9ff75514f9',
       'Content-Type': "application/json"

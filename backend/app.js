e23ad83f951c789577324f2efedf8afd243f9c3f
dimorl02 <dimorl02@gmail.com>
2023-11-13 18:07:08 +0300

    cors middleware

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const auth  = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const { validateLogin, validateCreateUser } = require('./middlewares/validation');
-const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('./utils/constants');
+const cors  = require('./middlewares/cors');
 
 
 const { NotFoundError } = require('./errors/NotFoundError');
@@ -15,22 +15,7 @@ cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  const { method } = req;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    if (method === 'OPTIONS') {
-      // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      const requestHeaders = req.headers['access-control-request-headers'];
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-    }
-  }
-  return next();
-});
+app.use(cors);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 }).then(() => console.log('Connected to MongoDB'));
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,65 +1,62 @@
 export const BASE_URL = 'https://api.mesto.dimorl02.nomoredomainsrocks.ru';
 
 function checkResponseValidity(res) {
-    if (res.ok) {
-      return res.json();
-    } 
-    return Promise.reject(`Ошибка: ${res.status}`);
-  };
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+};
 
 
-export function registerUser(email, password) {
-    return fetch(`${BASE_URL}/signup`, {
-      method: 'POST',
-      headers: {
-        "Accept": "application/json",
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    })
+export const registerUser = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
     .then(console.log(email, password))
-      .then(res => checkResponseValidity(res));
-  };
-  
-  export function authorizeUser(email, password) {
-    return fetch(`${BASE_URL}/signin`, {
-      method: 'POST',
-      headers: {
-        "Accept": "application/json",
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
+    .then(res => checkResponseValidity(res));
+};
+
+export const authorizeUser = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "password": password,
+      "email": email
+    }),
+  })
+    .then((res) => {
+      return checkResponseValidity(res);
     })
-      .then(res => checkResponseValidity(res))
-      .then((data) => {
-        if (data.token) {
-          const token = data.token;
-          localStorage.setItem('jwt', token);
-          return token;
-        };
-      })
-  };
+};
 
 export const checkToken = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    }
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+      }
   })
-  .then(res => res.json())
-  .then(data => data)
+      .then((res) => {
+          return checkResponse(res);
+      })
 }
 
-export function getContent(token) {
-    return fetch(`${BASE_URL}/users/me`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      }
-    })
-      .then(res => checkResponseValidity(res))
-      .then(data => data)
-  };
+export const getContent =(token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  })
+    .then(res => checkResponseValidity(res))
+    .then(data => data)
+};

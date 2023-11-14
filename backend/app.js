4b660ec81083a69fc3b13082b98fa52ba8feee73
dimorl02 <dimorl02@gmail.com>
2023-11-14 13:37:21 +0300

    cors packages

--- backend/app.js
+++ backend/app.js
@@ -7,21 +7,23 @@ const errorHandler = require('./middlewares/errorHandler');
 const auth  = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const { validateLogin, validateCreateUser } = require('./middlewares/validation');
-const cors  = require('./middlewares/cors');
+const cors  = require('cors');
 
 
 const { NotFoundError } = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use(cors);
+
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 }).then(() => console.log('Connected to MongoDB'));
+app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/signin', validateLogin, login);
 app.use('/signup', validateCreateUser, createUser);
-app.use(helmet());
+
 
 app.use(auth);
 app.use('/users', require('./routes/users'));
--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -3,7 +3,6 @@ class Api {
     constructor(options) {
         this._url = options.url;
         this._headers = options.headers;
-
     }
 
     _checkResponseValidity(res) {

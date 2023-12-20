af61ce1bba311940b1a53dd44c1442de1d73a881
dimorl02 <dimorl02@gmail.com>
2023-12-20 18:13:27 +0300

    замена адресов 3.0

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,11 +6,7 @@ const allowedCors = [
   'https://localhost:3000',
   'http://localhost:3000',
   'https://localhost:3001',
-  'http://localhost:3001',
-  'https://localhost:3002',
-  'http://localhost:3002',
-  'https://localhost:3003',
-  'http://localhost:3003',
+  'http://localhost:3001'
 ];
 
 module.exports = (req, res, next) => {
